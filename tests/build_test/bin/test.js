c51632683a82baab7a3cacae6a1bef76d639a57f
Ashutosh Varma <ashutoshvarma11@live.com>
2021-05-05 22:16:08 +0530

    fix: now tests passes

--- tests/build_test/bin/test.js
+++ tests/build_test/bin/test.js
@@ -1,8 +1,12 @@
-let sh = require('shelljs');
-let assert = require('assert');
+let sh = require("shelljs");
+let assert = require("assert");
 
 const hello = "hello world";
-let res = sh.exec("../../../bin/asb run ./build/release/bin.wasm -- " + hello, {silent: true}).stdout;
+// build WASI binary
+sh.exec("../../../bin/asb", { silent: true }).stdout;
+let res = sh.exec("../../../bin/asb run ./build/release/bin.wasm -- " + hello, {
+  silent: true,
+}).stdout;
 
-assert(res.startsWith(hello))
+assert(res.startsWith(hello));
 console.log("Passed echo test");
